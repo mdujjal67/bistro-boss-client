d182d6210c792de0dc0c9ea5123bb97b4f8e8db9
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-20 11:32:53 +0600

    project setup and simple footer setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500'>Bistro Boss</h1>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
