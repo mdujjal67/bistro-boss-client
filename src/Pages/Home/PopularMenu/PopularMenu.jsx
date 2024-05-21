7e00bf1e288e410fe028f8ab539340bb8e5b3cc2
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-21 10:24:03 +0600

    added menu.json data and popular menu item section

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
+
+    const [menu, setMenu] = useState([])
+    useEffect (() => {
+        fetch("menu.json")
+        .then(res => res.json())
+        .then(data => {
+            const popularItems = data.filter(item => item.category === 'popular');
+            setMenu(popularItems);
+        })
+    },[])
     return (
-        <section>
+        <section className="mb-[100px]">
             <SectionTitle
                 heading ="from our menu"
                 subHeading="Popular Items"
             ></SectionTitle>
+            <div className="grid md:grid-cols-2 gap-6">
+                {
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item = {item}
+                    ></MenuItem>)
+                }
+            </div>
         </section>
     );
 };
---
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,18 @@
+import PropType from "prop-types"
+const MenuItem = ({item}) => {
+    const {image, recipe, name, price} = item
+    return (
+        <div className="flex items-center space-x-5">
+            <img style={{borderRadius: "0 200px 200px 200px"}} src={image} alt="" className="w-[110px] rounded-full"/>
+            <div>
+                <h3>{name}----------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-[#BB8506]">${price}</p>
+        </div>
+    );
+};
+MenuItem.propTypes ={
+    item: PropType.object,
+}
+export default MenuItem;
