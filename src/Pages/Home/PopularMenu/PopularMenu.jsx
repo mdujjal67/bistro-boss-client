d889d75a6f713db684c88a59ed035f05cba1047f
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-21 23:14:36 +0600

    added custome hook for menu page

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../Hooks/useMenu";
 
 const PopularMenu = () => {
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
 
-    const [menu, setMenu] = useState([])
-    useEffect (() => {
-        fetch("menu.json")
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular');
-            setMenu(popularItems);
-        })
-    },[])
+    // const [menu, setMenu] = useState([])
+    // useEffect (() => {
+    //     fetch("menu.json")
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const popularItems = data.filter(item => item.category === 'popular');
+    //         setMenu(popularItems);
+    //     })
+    // },[])
     return (
         <section className="mb-[100px]">
             <SectionTitle
@@ -21,7 +24,7 @@ const PopularMenu = () => {
             ></SectionTitle>
             <div className="grid md:grid-cols-2 gap-6">
                 {
-                    menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
                         key={item._id}
                         item = {item}
                     ></MenuItem>)
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -2,8 +2,17 @@ import { Helmet } from "react-helmet-async";
 import CoverContent from "../../Shared/CoverContent/CoverContent";
 import menuImg from "../../../assets/menu/banner3.jpg"
 import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import useMenu from "../../../Hooks/useMenu";
 
 const Menu = () => {
+
+    const [menu] = useMenu();
+    const dessert = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+
     return (
         
         <div className="">
@@ -13,15 +22,6 @@ const Menu = () => {
             </Helmet>
 
             <CoverContent img={menuImg} title="our menu" content="Would you like to try a dish?"></CoverContent>
-            <PopularMenu></PopularMenu>
-            <CoverContent img={menuImg} title="our menu" content="Would you like to try a dish?"></CoverContent>
-            <PopularMenu></PopularMenu>
-            <CoverContent img={menuImg} title="our menu" content="Would you like to try a dish?"></CoverContent>
-            <PopularMenu></PopularMenu>
-            <CoverContent img={menuImg} title="our menu" content="Would you like to try a dish?"></CoverContent>
-            <PopularMenu></PopularMenu>
-            <CoverContent img={menuImg} title="our menu" content="Would you like to try a dish?"></CoverContent>
-            <PopularMenu></PopularMenu>
         </div>
     );
 };
